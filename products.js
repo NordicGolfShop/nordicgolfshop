1e07fb71e1db6e8cd44fc2c3ffbbbbe262c84d60
NordicGolfShop <nordicgolfshop@gmail.com>
2026-01-20 14:32:31 +0100

    Update products.js

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products = [
     stock: 3,
     image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
     qr: "https://i.postimg.cc/02dn8xdn/swish-QR-small-(3).png",
-    description: "En kraftfull driver för långa slag."
+    description: "Ett packet med 8 stycken golfbollar."
   },
   {
     id: "bollar-002",
@@ -15,7 +15,7 @@ const products = [
     stock: 2,
     image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
     qr: "https://i.postimg.cc/T1DBCPSq/swish-QR-small.png",
-    description: "Perfekt järnklubba för kontroll och precision."
+    description: "Ett packet med 12 stycken golfbollar."
   },
   {
     id: "bollar-003",
@@ -24,6 +24,6 @@ const products = [
     stock: 1,
     image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
     qr: "https://i.postimg.cc/Yqr722B9/golfbollar20.png",
-    description: "En klassisk putter för stabila puttar."
+    description: "Ett packet med 20 stycken golfbollar."
   }
 ];
