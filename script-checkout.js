a6e0e4bc7c6cdfc19d370d85e16d381bb241af57
NordicGolfShop <nordicgolfshop@gmail.com>
2026-01-20 11:44:26 +0100

    Update script-checkout.js

--- script-checkout.js
+++ script-checkout.js
@@ -9,33 +9,37 @@ const swishText = document.getElementById("swishText");
 infoForm.addEventListener("submit", function(e){
   e.preventDefault();
 
-  // Hämta kundinfo
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const address = document.getElementById("address").value;
-
   if(!selected){
     alert("Ingen produkt vald.");
     return;
   }
 
+  // Hämta kundinfo
+  const phone = document.getElementById("phone").value;
+  const fullname = document.getElementById("fullname").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
+  const city = document.getElementById("city").value;
+  const postcode = document.getElementById("postcode").value;
+
   // Skicka formulär via AJAX till Formspree
   const formData = new FormData();
-  formData.append("name", name);
-  formData.append("email", email);
-  formData.append("address", address);
-  formData.append("product", selected.name[currentLang]);
-  formData.append("price", selected.price);
+  formData.append("Betalande Telefonnummer", phone);
+  formData.append("Fulla Namn", fullname);
+  formData.append("Mail", email);
+  formData.append("Adress", address);
+  formData.append("Ort", city);
+  formData.append("Postkod", postcode);
+  formData.append("Produkt", selected.name[currentLang]);
+  formData.append("Pris", selected.price);
 
-  fetch("https://formspree.io/f/XXXXXX", {
+  fetch("https://formspree.io/f/mojjlbkr", {
     method: "POST",
     body: formData,
-    headers: {
-      'Accept': 'application/json'
-    }
+    headers: { 'Accept': 'application/json' }
   })
   .then(response => {
-    if (response.ok) {
+    if(response.ok){
       // Dölj kundinfo
       infoForm.style.display = "none";
 
