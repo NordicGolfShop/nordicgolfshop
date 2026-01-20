a675be604b3ca737eb6d1d98dcc92c29e86d4d80
NordicGolfShop <nordicgolfshop@gmail.com>
2026-01-20 11:32:14 +0100

    Create script-products.js

---
+++ script-products.js
@@ -0,0 +1,25 @@
+const productsEl = document.getElementById("products");
+
+function renderProducts() {
+  productsEl.innerHTML = "";
+  products.forEach(p => {
+    const div = document.createElement("div");
+    div.className = "product";
+    div.innerHTML = `
+      <img src="${p.qr}" alt="QR för ${p.name[currentLang]}">
+      <h3>${p.name[currentLang]}</h3>
+      <p>${p.price} SEK</p>
+      <p>${translations.stock[currentLang]}: ${p.stock}</p>
+      <button onclick="goToCheckout('${p.id}')">${translations.buy[currentLang]}</button>
+    `;
+    productsEl.appendChild(div);
+  });
+}
+
+function goToCheckout(id) {
+  const p = products.find(x => x.id === id);
+  localStorage.setItem("selectedProduct", JSON.stringify(p));
+  window.location.href = "checkout.html";
+}
+
+renderProducts();
