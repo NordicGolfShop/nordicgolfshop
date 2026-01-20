654b9c6c64fbe5162034161597905f2608a0711e
NordicGolfShop <nordicgolfshop@gmail.com>
2026-01-20 12:43:12 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: "driver-001",
+    name: "Golf Driver Pro",
+    price: 2999,
+    stock: 5,
+    qr: "https://i.postimg.cc/XXXX/driver-001.png",
+    description: "En kraftfull driver för långa slag."
+  },
+  {
+    id: "iron-002",
+    name: "Järnklubba Precision",
+    price: 1499,
+    stock: 8,
+    qr: "https://i.postimg.cc/XXXX/iron-002.png",
+    description: "Perfekt järnklubba för kontroll och precision."
+  },
+  {
+    id: "putter-003",
+    name: "Putter Classic",
+    price: 999,
+    stock: 12,
+    qr: "https://i.postimg.cc/XXXX/putter-003.png",
+    description: "En klassisk putter för stabila puttar."
+  }
+];
