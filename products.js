2d6934f3c2e6cb6af1abcd2eacf740add0101ce6
NordicGolfShop <nordicgolfshop@gmail.com>
2026-01-20 14:38:45 +0100

    Update products.js

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products = [
     stock: 3,
     image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
     qr: "https://i.postimg.cc/02dn8xdn/swish-QR-small-(3).png",
-    description: "Ett packet med 8 stycken golfbollar."
+    description: "Styckepris för 8 stycken golfbollar."
   },
   {
     id: "bollar-002",
@@ -15,7 +15,7 @@ const products = [
     stock: 2,
     image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
     qr: "https://i.postimg.cc/T1DBCPSq/swish-QR-small.png",
-    description: "Ett packet med 12 stycken golfbollar."
+    description: "Styckepris för 12 stycken golfbollar."
   },
   {
     id: "bollar-003",
@@ -24,6 +24,6 @@ const products = [
     stock: 1,
     image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
     qr: "https://i.postimg.cc/Yqr722B9/golfbollar20.png",
-    description: "Ett packet med 20 stycken golfbollar."
+    description: "Stryckepris för 20 stycken golfbollar."
   }
 ];
